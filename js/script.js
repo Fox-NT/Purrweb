003b0ae74ced7b355bea54e2eca7042c5c89d58f
Fox-NT <newtorka@gmail.com>
2021-03-18 14:20:22 +0600

    burger

--- js/script.js
+++ js/script.js
@@ -9,5 +9,15 @@ document.addEventListener('DOMContentLoaded', () => {
             console.log(window.pageYOffset);
 
     
-
+            const burger = document.querySelector('.burger'),
+            menu = document.querySelector('.burger_menu'),
+            list = document.querySelector('.burger_menu .nav_list'),
+            body = document.querySelector('body');
+    
+            burger.addEventListener('click', () => {
+                menu.classList.toggle('visible_mobile');
+                burger.classList.toggle('burger_active');
+                body.classList.toggle('over');
+                list.classList.toggle('nav_list_active');
+            });
 });
